e3a1c3fd4f2c42356b670d707fb712d0686cecee
Janne <janne@mainoslahde.fi>
2020-07-22 09:37:13 +0300

    Added bootstrap nav menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,29 +1,55 @@
 <header class="banner">
   <div class="container">
-    <a class="brand text-dark" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <!--<div id="desktop-nav">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']) !!}
       @endif
     </div>-->
-    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+    <nav class="navbar-inverse navbar-static-top navbar navbar-default navbar-expand-md" role="navigation">
       <div class="navbar-header">
-        <button id="navbar-toggler" class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-collapse"
-          aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
-          <div class="animated-icon">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </button>
+        <a class="navbar-brand text-dark" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
       </div>
+      
+      <button id="navbar-toggler" class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-collapse"
+        aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="animated-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+
+      <div class="collapse navbar-collapse navbar-ex1-collapse bg-dark" id="navbar-collapse"> 
+        
+        @if (has_nav_menu('primary_navigation'))
+          {!! 
+            wp_nav_menu( array(
+            'menu' => 'top_menu',
+            'depth' => 2,
+            'container' => false,
+            'menu_class' => 'nav navbar-nav',
+            //Process nav menu using our custom nav walker
+            'walker' => new wp_bootstrap_navwalker())
+          );
+          !!}
+        @endif
+
+      </div>
+      
     </nav>
   </div>
-  <div class="collapse navbar-collapse bg-dark" id="navbar-collapse">
-    <div class="container">
+  <div class="collapse navbar-collapse bg-dark">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']) !!}
+        {!! 
+          wp_nav_menu( array(
+          'menu' => 'top_menu',
+          'depth' => 2,
+          'container' => false,
+          'menu_class' => 'nav',
+          //Process nav menu using our custom nav walker
+          'walker' => new wp_bootstrap_navwalker())
+        );
+        !!}
       @endif
-    </div>
   </div>
 </header>
