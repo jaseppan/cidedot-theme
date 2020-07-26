8a738bacc2e2ff6ec3f0d58c43cd1f6fe8dc255e
Janne <janne@mainoslahde.fi>
2020-07-26 08:30:53 +0300

    Modified header and added paper background and paragraph font and other changes in css

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,6 +3,9 @@ export default {
     // JavaScript to be fired on all pages
     $('#navbar-toggler').on('click', function () {
       $('.animated-icon').toggleClass('open');
+      $('#site-identity').toggleClass('text-center');
+      $('#site-identity .divider').toggleClass('hidden');
+      $('#site-identity').toggleClass('collapsed');
     });
     $('.second-button').on('click', function () {
   
